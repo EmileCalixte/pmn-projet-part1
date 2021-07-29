41f92e4fb4f795c7a46ccc60b7bcc46795b4f56b
Emile Calixte <calixte.emile@gmail.com>
2021-07-29 10:13:56 +0200

    Configure SSH for root users

--- provision.sh
+++ provision.sh
@@ -7,47 +7,68 @@ set -u
 # Je récupere le hostname du serveur
 HOSTNAME="$(hostname)"
 
+# Je définis le contenu du fichier de configuration SSH
+SSH_CONFIG=$(cat << 'EOF'
+Host s0.infra
+        User vagrant
+        Hostname 192.168.50.20
+        IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+
+Host s1.infra
+        User vagrant
+        Hostname 192.168.50.30
+        IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+
+Host s2.infra
+        User vagrant
+        Hostname 192.168.50.40
+        IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+
+Host s3.infra
+        User vagrant
+        Hostname 192.168.50.50
+        IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+
+Host s4.infra
+        User vagrant
+        Hostname 192.168.50.60
+        IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+EOF
+)
+
 if [ "$HOSTNAME" = "control" ]; then
 	# J'installe ansible dessus
   apt-get install -y \
 		ansible
 
+  # Je configure SSH pour l'utilisateur vagrant
   cd /home/vagrant/.ssh
   cp /vagrant/ansible_deploy_nopass_rsa .
   chmod 600 ansible_deploy_nopass_rsa
   touch config
-  # shellcheck disable=SC1073
-  # shellcheck disable=SC1009
-  cat << EOF > config
-  Host s0.infra
-          User vagrant
-          Hostname 192.168.50.20
-          IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+  echo "$SSH_CONFIG" > config
+  chown -R vagrant:vagrant /home/vagrant/.ssh
 
-  Host s1.infra
-          User vagrant
-          Hostname 192.168.50.30
-          IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
+  # Je configure SSH pour l'utilisateur root
+  cd /root
+  mkdir .ssh
+  cd .ssh
+  cp /vagrant/ansible_deploy_nopass_rsa .
+  chmod 600 ansible_deploy_nopass_rsa
+  touch config
+  echo "$SSH_CONFIG" > config
 
-  Host s2.infra
-          User vagrant
-          Hostname 192.168.50.40
-          IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
-
-  Host s3.infra
-          User vagrant
-          Hostname 192.168.50.50
-          IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
-
-  Host s4.infra
-          User vagrant
-          Hostname 192.168.50.60
-          IdentityFile ~/.ssh/ansible_deploy_nopass_rsa
-EOF
-chown -R vagrant:vagrant /home/vagrant/.ssh
 else
+  # Je configure SSH pour l'utilisateur vagrant
   cd /home/vagrant/.ssh
   cp /vagrant/ansible_deploy_nopass_rsa.pub .
   cat ansible_deploy_nopass_rsa.pub >> authorized_keys
   chown -R vagrant:vagrant /home/vagrant/.ssh
+
+  # Je configure SSH pour l'utilisateur root
+  cd /root
+  mkdir .ssh
+  cd .ssh
+  cp /vagrant/ansible_deploy_nopass_rsa.pub .
+  cat ansible_deploy_nopass_rsa.pub >> authorized_keys
 fi
